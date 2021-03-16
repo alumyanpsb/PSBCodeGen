fa209ff584f3978a3f0d71350dd87bb1ab9ba4fc
Alumyan Armen Sevadaevich <alumyanas@psbank.ru>
2021-03-16 15:30:59 +0300

    for test

--- gen.sh
+++ gen.sh
@@ -8,14 +8,14 @@ echo "Загрузка api.json"
 curl -s http://retail-tst.payment.ru/api/swagger/docs/v1 > api.json &
 wait
 echo "Загрузка генератора"
-curl -s https://raw.githubusercontent.com/kekcik/PSBCodeGen/master/main.js > main.js &
+curl -s https://raw.githubusercontent.com/alumyanpsb/PSBCodeGen/feature/ca_object_test/main.js > main.js &
 wait
 echo "Загрузка негенерирумеых файлов"
-curl -s https://raw.githubusercontent.com/kekcik/PSBCodeGen/master/CommonApi.swift > Core/CommonApi.swift &
+curl -s https://raw.githubusercontent.com/alumyanpsb/PSBCodeGen/feature/ca_object_test/CommonApi.swift > Core/CommonApi.swift &
 wait
-curl -s https://raw.githubusercontent.com/kekcik/PSBCodeGen/master/Error+Alamofire.swift > Core/Error+Alamofire.swift &
+curl -s https://raw.githubusercontent.com/alumyanpsb/PSBCodeGen/feature/ca_object_test/Error+Alamofire.swift > Core/Error+Alamofire.swift &
 wait
-curl -s https://raw.githubusercontent.com/kekcik/PSBCodeGen/master/CAObject.swift > Core/CAObject.swift &
+curl -s https://raw.githubusercontent.com/alumyanpsb/PSBCodeGen/feature/ca_object_test/CAObject.swift > Core/CAObject.swift &
 wait
 echo "Генерация методов"
 node main.js
